3a281d76ba5e61ec76d2b97a28b5d23089c18e8a
Gerwyn George <62806334+Gerwyn-George@users.noreply.github.com>
2023-07-02 16:31:07 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -231,480 +231,72 @@ function _install_web_server ()
 
     apt install apache2 -y > /dev/null
 
-    service apache2 start
-
-
-    touch /var/www/html/home.html
-
-    cat > /var/www/html/home.html << EOF
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>login</title>
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <style>
-        .header{
-            position: relative;
-            left: 75px;
-            padding-bottom: 40px;
-            text-align: center;
-        }
-
-        .text{
-            position: relative;
-            font-size: 4ch;
-            left: 25px;
-            text-align: center;
-            border: 5px black;
-        }
-
-        .bottom{
-            position:relative;
-            text-align: center;
-            top: 825px;
-            left: 75px;
-        }
-    </style>
-      </head>
-  <body>
-
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Hogwarts</span>
-          </a>
-
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="home.html" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="About.html" class="nav-link">About</a></li>
-            <li class="nav-item"><a href="opening hours.html" class="nav-link">opening hours</a></li>
-            <li class="nav-item"><a href="Contact_Us.html" class="nav-link">Contact Us</a></li>
-            <li class="nav-item"><a href="Login.html" class="nav-link">Login</a></li>
-          </ul>
-        </header>
-      </div>
-
-    <h1 class="header">Welcome</h1>
-
-    <div class="text">
-        <p> Welcome to the Hogwarts homepage and thank you for visiting our site.</p>
-        <p>Please use the navigation bar at the top of the webpage to navigate around the website.</p>
-        <p>We hope that you will enjoy your time here.</p>
-    </div>
-
-    <div class="bottom">
-        <p>This site was created by Dobby the house elf. - 2023</p>
-    </div>
-
-  </body>
-</html>
-
-
-
-EOF
-
-    touch /var/www/html/Contact_Us.html
-
-    cat > /var/www/html/Contact_Us << EOF
-
-    <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>login</title>
-    <link href="css/bootstrap.css" rel="stylesheet">
-  <style>
-
-    .title{
-      position: relative;
-      left: 1200px;
-      top: 40px;
-      padding-bottom: 40px;
-
-    }
-    .contacts_list{
-      position: relative;
-      left: 650px;
-      top: 75px;
-
-      width: 1200px;
-    }
-
-    .bottom{
-      position:relative;
-      text-align: center;
-      top: 850px;
-      left: 75px;
-
-        }
-
-
-  </style>
-
-  </head>
-
-  <body>
-
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Hogwarts</span>
-          </a>
-
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="home.html" class="nav-link" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="About.html" class="nav-link">About</a></li>
-            <li class="nav-item"><a href="opening hours.html" class="nav-link">opening hours</a></li>
-            <li class="nav-item"><a href="Contact_Us.html" class="nav-link active">Contact Us</a></li>
-            <li class="nav-item"><a href="Login.html" class="nav-link">Login</a></li>
-          </ul>
-        </header>
-      </div>
-
-      <h1 class="title">Contacts</h1>
-
-      <div class="contacts_list">
-        <table class="table">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">email address</address></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Harry</td>
-              <td>Potter</td>
-              <td>Harry.Potter@hogwart.ac.uk</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Hagrid</td>
-              <td></td>
-              <td>Hargrid@hogwarts.ac.uk</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Servus</td>
-              <td>Snape</td>
-              <td>Servus.Snape@hogwarts.ac.uk</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <div class="bottom">
-        <p>This site was created by Dobby the house elf. - 2023</p>
-      </div>
-
-  </body>
-</html>
-
-
-EOF
-
-    touch /var/www/html/Login.html
-
-    cat > /var/www/html/Login.html << EOF
-
-    <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>login</title>
-    <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet">
-  <style>
-  .header {
-    position: relative;
-    left: 65px;
-    padding-bottom: 40px;
-    text-align: center;
-  }
-
-  .login_box {
-    background-color: white;
-    position: relative;
-    left: 1125px;
-  }
-
-  .bottom{
-        position:relative;
-        text-align: center;
-        top: 818px;
-        left: 75px;
-      }
-  </style>
-
-  </head>
-  <body>
-
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Hogwarts</span>
-          </a>
-
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="home.html" class="nav-link" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="About.html" class="nav-link">About</a></li>
-            <li class="nav-item"><a href="opening hours.html" class="nav-link">opening hours</a></li>
-            <li class="nav-item"><a href="Contact_Us.html" class="nav-link">Contact Us</a></li>
-            <li class="nav-item"><a href="Login.html" class="nav-link active">Login</a></li>
-          </ul>
-        </header>
-      </div>
-
-    <h1 class="header">Login Page</h1>
-
-    <div class="login_box">
-        <form method="post" action="login.php">
-            <div class="mb-3" style="width:450px;">
-                <label for="exampleInputEmail1" class="form-label">Username</label>
-                <input type="text" name=username class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3" style="width:450px;">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" name=password class="form-control" id="exampleInputPassword1">
-            </div>
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-
-
-<?php
-if(isset($_POST['submit']))
-{
-
-        $servername = "localhost";
-        $username = "gerwyn";
-        $password = "";
-        $database = "exploitable";
-
-        $conn = mysqli_connect($servername, $username, $password,$database);
-
-        if (!$conn){
-           die("connection failed:" . mysqli_connect_error());
-        }
-
-        try{
-
-        $lusername = $_POST['username'];
-        $lpassword = $_POST['password'];
-
-        $lquery= "SELECT * FROM accounts WHERE username='".
-        $lusername .
-        "' AND password='" .
-        $lpassword .
-        "'";
-                $lEncodeOutput = False;
-
-        $result =  $conn->query($lquery);
-                if(!$result){
-                throw (new Exception('Error executing query: '.$conn->error, $conn->errorno));
-                }
-
-                if ($result->num_rows > 0) {
-                        echo '<p> Results for '.$row->username.'. '.$result->num_rows.' record found.<p>';
-                while($row = $result->fetch_object()){
-                if (!$lEndcodeOutput){
-                        $lusername = $row->username;
-                        $lpassword = $row->password;
-                }else{
-                        $lusername = $Encoder->encodeforHTML($row->username);
-                        $lpassword = $Encoder->encodeforHTML($row->password);
-                }
-
-                echo "<b>Username=</b>{$lusername}<br>";
-                echo "<b>Password=</b>{$lpassword}<br>";
-        }
-        echo "<p>";
-        } else {
-        echo "fail";
-        }
-
-} catch (Exception $e) {
-        echo $e;
-}
-
-}
-?>
-
-    <div class="bottom">
-      <p>This site was created by Dobby the house elf. - 2023</p>
-    </div>
-
-  </body>
-</html>
-
-EOF
-
-
-    touch /var/www/html/About.html
-
-    cat > /var/www/html/About.html << EOF
-
-    <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>login</title>
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <style>
-      .header{
-        position: relative;
-        left: 75px;
-        padding-bottom: 40px;
-        text-align: center;
-
-      }
-
-      .text{
-        position: relative;
-        font-size: 4ch;
-        left: 25px;
-        text-align: center;
-
-        border: 5px black;
-
-      }
-
-      .bottom{
-        position:relative;
-        text-align: center;
-        top: 825px;
-        left: 75px;
-
-      }
-
-    </style>
-
-  </head>
-  <body>
-
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Hogwarts</span>
-          </a>
-
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="home.html" class="nav-link" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="About.html" class="nav-link active">About</a></li>
-            <li class="nav-item"><a href="opening hours.html" class="nav-link">opening hours</a></li>
-            <li class="nav-item"><a href="Contact_Us.html" class="nav-link">Contact Us</a></li>
-            <li class="nav-item"><a href="Login.html" class="nav-link">Login</a></li>
-          </ul>
-        </header>
-      </div>
-
-    <h1 class="header">About</h1>
-
-    <div class="text">
-      <p> Hogwarts School of Witchcraft and Wizardry is a boarding school.</p>
-      <p>For young witches and wizards that was founded around the 9th and 10 centry</p>
-      <p>by Godric Gryffindor, Rowena Ravenclaw, Helga Hufflepuff and Salazar Slytherin.</p>
-  </div>
-
-  <div class="bottom">
-    <p>This site was created by Dobby the house elf. - 2023</p>
-  </div>
-
-
-  </body>
-</html>
-
-
-EOF
-
-    touch /var/www/html/opening hours.html
-
-    cat > /var/www/html/opening hours.html << EOF
-
-    <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>login</title>
-    <link href="css/bootstrap.css" rel="stylesheet">
-  <style>
-    .header{
-        position: relative;
-        left: 75px;
-        padding-bottom: 40px;
-        text-align: center;
-      }
-
-    .text{
-      position: relative;
-      font-size: 2ch;
-      left: 25px;
-      text-align: center;
-      border: 5px black;
-    }
-
-    .bottom{
-      position:relative;
-      text-align: center;
-      top: 818px;
-      left: 75px;
-    }
-
-  </style>
-
-  </head>
-  <body>
-
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-            <span class="fs-4">Hogwarts</span>
-          </a>
-
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="home.html" class="nav-link" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="About.html" class="nav-link">About</a></li>
-            <li class="nav-item"><a href="opening hours.html" class="nav-link active">opening hours</a></li>
-            <li class="nav-item"><a href="Contact_Us.html" class="nav-link">Contact Us</a></li>
-            <li class="nav-item"><a href="Login.html" class="nav-link">Login</a></li>
-          </ul>
-        </header>
-      </div>
-
-      <h1 class="header">Opening hours</h1>
-
-    <div class="text">
-      <p>Saturday</p>
-      <p>Monday</p>
-      <p>Tuesday</p>
-      <p>Wednesday</p>
-      <p>Thursday</p>
-      <p>Friday</p>
-      <p>Sunday</p>
-    </div>
-
-    <div class="bottom">
-      <p>This site was created by Dobby the house elf. - 2023</p>
-    </div>
-
-  </body>
-</html>
-
-EOF
+    #Welcome page
+
+    cp /home/gerwyn/home.html /var/www/html/home.html
+
+    sed -i "s/SITE_NAME/$SITE_NAME/" /var/www/html/home.html
+    sed -i "s/WELCOME_MESSAGE/$WELCOME/" /var/www/html/home.html
+    sed -i "s/WEBSITE_CREATED_BY/$WEBSITE_CREATED_BY/" /var/www/html/home.html
+
+    #About page
+
+    cp /home/gerwyn/About.html /var/www/html/About.html
+
+    sed -i "s/SITE_NAME/$SITE_NAME/" /var/www/html/About.html
+    sed -i "s/WEBSITE_CREATED_BY/$WEBSITE_CREATED_BY/" /var/www/html/About.html
+
+    #Opening hours page
+
+    cp "/home/gerwyn/opening hours.html" "/var/www/html/opening hours.html"
+
+    sed -i "s/SITE_NAME/$SITE_NAME/" "/var/www/html/opening hours.html"
+    sed -i "s/WEBSITE_CREATED_BY/$WEBSITE_CREATED_BY/" "/var/www/html/opening hours.html"
+
+
+    #Login page
+
+    #cp "/home/gerwyn/login.html" "/var/www/html/login.html
+
+    #sed -i "/SITE_NAME/$SITE_NAME/" /var/www/html/login.html
+    #sed -i "/WEBSITE_CREATED_BY/$WEBSITE_CREATED_BY/" /var/www/html/login.html
+
+
+    #Contact Us page set up.
+
+    cp /home/gerwyn/Contact_Us.html /var/www/html/Contact_Us.html
+
+    sed -i "s/SITE_NAME/$SITE_NAME/" /var/www/html/Contact_Us.html
+
+    sed -i "s/ROW_ONE_FIRSTNAME/$db_character_one_firstname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_ONE_SURNAME/$db_character_one_lastname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_ONE_EMAIL/$CONTACT_EMAIL_ONE/" /var/www/html/Contact_Us.html
+
+    sed -i "s/ROW_TWO_FIRSTNAME/$db_character_two_firstname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_TWO_SURNAME/$db_character_two_lastname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_TWO_EMAIL/$CONTACT_EMAIL_TWO/" /var/www/html/Contact_Us.html
+
+    sed -i "s/ROW_THREE_FIRSTNAME/$db_character_three_firstname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_THREE_SURNAME/$db_character_three_lastname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_THREE_EMAIL/$CONTACT_EMAIL_THREE/" /var/www/html/Contact_Us.html
+
+    sed -i "s/ROW_FOUR_FIRSTNAME/$db_character_four_firstname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_FOUR_SURNAME/$db_character_four_lastname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_FOUR_EMAIL/$CONTACT_EMAIL_FOUR/" /var/www/html/Contact_Us.html
+
+    sed -i "s/ROW_FIVE_FIRSTNAME/$db_character_five_firstname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_FIVE_SURNAME/$db_character_five_lastname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_FIVE_EMAIL/$CONTACT_EMAIL_FIVE/" /var/www/html/Contact_Us.html
+
+    sed -i "s/ROW_SIX_FIRSTNAME/$db_character_six_firstname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_SIX_SURNAME/$db_character_six_lastname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_SIX_EMAIL/$CONTACT_EMAIL_SIX/" /var/www/html/Contact_Us.html
+
+    sed -i "s/ROW_SEVEN_FIRSTNAME/$db_character_seven_firstname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_SEVEN_SURNAME/$db_character_seven_lastname/" /var/www/html/Contact_Us.html
+    sed -i "s/ROW_SEVEN_EMAIL/$CONTACT_EMAIL_SEVEN/" /var/www/html/Contact_Us.html
+
+    sed -i "s/WEBSITE_CREATED_BY/$WEBSITE_CREATED_BY/" /var/www/html/Contact_Us.html
 
     service apache2 start
 
@@ -753,9 +345,6 @@ function _install_bootstrap ()
 
     cp -r /home/gerwyn/bootstrap/bootstrap-5.1.3/scss /var/www/html/bootstrap/
 }
-
-
-
 
 
 _set_hostname
