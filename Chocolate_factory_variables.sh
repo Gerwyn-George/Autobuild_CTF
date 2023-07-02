9fe39bba0bd5ac0190089132e5a2fb7ca06f9e5b
Gerwyn George <62806334+Gerwyn-George@users.noreply.github.com>
2023-07-02 16:35:30 +0100

    Create Chocolate_factory_variables.sh

---
+++ Chocolate_factory_variables.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+#CTF Machine details
+
+HOSTNAME="Chocolate_Factory"
+IP="10.0.20.4"
+GATEWAY_IP=""
+
+#character information for the user creation.
+character_one="Willy"
+character_two="Charlie"
+character_three="Grandpa"
+character_four="William"
+character_five="Hellen"
+character_six="Oompa_Lompa"
+character_seven="Squrriel"
+
+password_one="password123"
+password_two="password123"
+password_three="password123"
+password_four="password123"
+password_five="password123"
+password_six="password123"
+password_seven="password123"
+
+#Here is the Database information variables
+
+#Character_one
+
+db_character_one_user="Willy"
+db_character_one_password="password123"
+db_character_one_is_admin="TRUE"
+db_character_one_firstname="Willy"
+db_character_one_lastname="Wonka"
+db_character_one_signature="I, Willy Wonka, have decided to allow five children - just five, mind you, and no more - to visit my factory this year."
+
+#Character_two#
+
+db_character_two_user="Charlie"
+db_character_two_password="password123"
+db_character_two_is_admin="FALSE"
+db_character_two_firstname="Charlie"
+db_character_two_lastname="Bucket"
+db_character_two_signature="I\'m here to try and win the Golden Ticket, sir."
+
+
+#Character_three
+db_character_three_user="Grandpa"
+db_character_three_password="password123"
+db_character_three_is_admin="FALSE"
+db_character_three_firstname="Grandpa"
+db_character_three_lastname="Joe"
+db_character_three_signature="I/'ve heard that what you imagine sometimes comes true."
+
+#Character_four
+db_character_four_user="William"
+db_character_four_password="password123"
+db_character_four_is_admin="FALSE"
+db_character_four_firstname="William"
+db_character_four_lastname="Bucket"
+db_character_four_signature="Well, I/'m Charlies dad."
+
+
+#Character_five
+db_character_five_user="Hellen"
+db_character_five_password="password123"
+db_character_five_is_admin="FALSE"
+db_character_five_firstname="Hellen"
+db_character_five_lastname="Bucket"
+db_character_five_signature="And i/'m Charlies mum."
+
+
+#Character_six
+db_character_six_user="Ommpa_Loompa"
+db_character_six_password="password123"
+db_character_six_is_admin="TRUE"
+db_character_six_firstname="Oompa"
+db_character_six_lastname="Loompa"
+db_character_six_signature="Oompa loompa doompety doo."
+
+#Character_seven
+db_character_seven_user="Squirrel"
+db_character_seven_password="password123"
+db_character_seven_is_admin="FALSE"
+db_character_seven_firstname="Squirrel"
+db_character_seven_lastname=""
+db_character_seven_signature="Squeak!"
+
+
+#Webserver variables
