37c5616f66cee629f1d0e5f6c9f5aa61e61ad564
Gerwyn George <62806334+Gerwyn-George@users.noreply.github.com>
2023-07-02 16:36:35 +0100

    Create xmen_variables.sh

---
+++ xmen_variables.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+#CTF Machine details
+
+HOSTNAME="Xaviers_School_for_Gifted_Youngsters"
+IP="10.0.20.4"
+GATEWAY_IP=""
+
+#character information for the user creation.
+character_one="Professor_X"
+character_two="Wolverine"
+character_three="Jean_Grey"
+character_four="Rouge"
+character_five="Cyclops"
+character_six="Storm"
+character_seven="Beast"
+
+password_one="password123"
+password_two="password123"
+password_three="password123"
+password_four="password123"
+password_five="password123"
+password_six="password123"
+password_seven="password123"
+
+#Here is the Database information variables
+
+#Character_one
+
+db_character_one_user="Professor_X"
+db_character_one_password="password123"
+db_character_one_is_admin="TRUE"
+db_character_one_firstname="Charles"
+db_character_one_lastname="Xavier"
+db_character_one_signature="You have the chance to become part of something much bigger than yourself."
+
+#Character_two#
+
+db_character_two_user="Wolverine"
+db_character_two_password="password123"
+db_character_two_is_admin="FALSE"
+db_character_two_firstname="James"
+db_character_two_lastname="Howlett"
+db_character_two_signature="My name is Logan."
+
+
+#Character_three
+db_character_three_user="Jean_Grey"
+db_character_three_password="password123"
+db_character_three_is_admin="FALSE"
+db_character_three_firstname="Jean"
+db_character_three_lastname="Grey"
+db_character_three_signature="I know who I am now.. I evolved beyond this world."
+
+#Character_four
+db_character_four_user="Rogue"
+db_character_four_password="password123"
+db_character_four_is_admin="FALSE"
+db_character_four_firstname="Anna"
+db_character_four_lastname="LeBeau"
+db_character_four_signature="Don\'t mess with the Rogue."
+
+
+#Character_five
+db_character_five_user="Cyclops"
+db_character_five_password="password123"
+db_character_five_is_admin="FALSE"
+db_character_five_firstname="Scott"
+db_character_five_lastname="Summers"
+db_character_five_signature="I can beat you with my eyes closed!"
+
+
+#Character_six
+db_character_six_user="Storm"
+db_character_six_password="password123"
+db_character_six_is_admin="FALSE"
+db_character_six_firstname="Ororo"
+db_character_six_lastname="Munroe"
+db_character_six_signature="Now you understand the true power of the storm!"
+
+#Character_seven
+db_character_seven_user="Beast"
+db_character_seven_password="password123"
+db_character_seven_is_admin="FALSE"
+db_character_seven_firstname="Henry"
+db_character_seven_lastname="McCoy"
+db_character_seven_signature="It\'s not animal, it'\s Beast."
+
+
+#Webserver variables
