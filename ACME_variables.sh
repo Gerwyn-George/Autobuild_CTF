2383e31b75ccf66edba1a23a3416b7d26f6cc951
Gerwyn George <62806334+Gerwyn-George@users.noreply.github.com>
2023-07-02 16:34:36 +0100

    Create ACME_variables.sh

---
+++ ACME_variables.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+
+#CTF Machine details
+
+HOSTNAME="ACME.INC"
+IP="10.0.20.4"
+GATEWAY_IP=""
+
+#character information for the user creation.
+character_one="Bugs"
+character_two="Lola"
+character_three="Daffy"
+character_four="Road_Runner"
+character_five="Porky"
+character_six="Wile_E"
+character_seven="Foghorn"
+
+password_one="password123"
+password_two="password123"
+password_three="password123"
+password_four="password123"
+password_five="password123"
+password_six="password123"
+password_seven="password123"
+
+#Here is the Database information variables
+
+#Character_one
+
+db_character_one_user="Bugs"
+db_character_one_password="password123"
+db_character_one_is_admin="FALSE"
+db_character_one_firstname="Bugs"
+db_character_one_lastname="Bunny"
+db_character_one_signature="Eh, What\'s up doc?"
+
+#Character_two#
+
+db_character_two_user="Lola"
+db_character_two_password="password123"
+db_character_two_is_admin="FALSE"
+db_character_two_firstname="Lola"
+db_character_two_lastname="Bunny"
+db_character_two_signature="Don't ever call me \'Doll."
+
+
+#Character_three
+db_character_three_user="Daffy"
+db_character_three_password="password123"
+db_character_three_is_admin="FALSE"
+db_character_three_firstname="Daffy"
+db_character_three_lastname="Duck"
+db_character_three_signature="I think you\'re pretty tough don\'t I?"
+
+#Character_four
+db_character_four_user="Road"
+db_character_four_password="password123"
+db_character_four_is_admin="FALSE"
+db_character_four_firstname="Road"
+db_character_four_lastname="Runner"
+db_character_four_signature="Meep! Meep!"
+
+
+#Character_five
+db_character_five_user="Porky"
+db_character_five_password="password123"
+db_character_five_is_admin="FALSE"
+db_character_five_firstname="Porky"
+db_character_five_lastname="Pig"
+db_character_five_signature="That\'s all folks!"
+
+
+#Character_six
+db_character_six_user="Wile_E"
+db_character_six_password="password123"
+db_character_six_is_admin="TRUE"
+db_character_six_firstname="Wile E"
+db_character_six_lastname="Coyote"
+db_character_six_signature="*HOLDS UP A SIGN WITH THE WORDS \'HELP\'*."
+
+#Character_seven
+db_character_seven_user="Foghorn"
+db_character_seven_password="password123"
+db_character_seven_is_admin="FALSE"
+db_character_seven_firstname="Foghorn"
+db_character_seven_lastname="Leghorn"
+db_character_seven_signature="I looka, I say, looka here."
+
+
+#Webserver variables
