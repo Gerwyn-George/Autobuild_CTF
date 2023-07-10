ced007f04524c07d35947f336f095beec5723f81
Gerwyn George <62806334+Gerwyn-George@users.noreply.github.com>
2023-07-10 18:55:37 +0100

    Update Chocolate_factory_variables.sh

--- Chocolate_factory_variables.sh
+++ Chocolate_factory_variables.sh
@@ -1,5 +1,134 @@
 #!/usr/bin/env bash
 
+
+#CTF Machine details
+
+HOSTNAME="ACME.INC"
+IP="10.0.20.4"
+GATEWAY_IP=""
+
+#character information for the user creation.
+character_one="Bugs"
+character_two="Lola"
+character_three="Daffy"
+character_four="Road_Runner"
+character_five="Porky"
+character_six="Wile_E"
+character_seven="Foghorn"
+
+password_one='password123'
+password_two='password123'
+password_three='password123'
+password_four='password123'
+password_five='password123'
+password_six='password123'
+password_seven='password123'
+
+#Here is the Database information variables
+
+#Character_one
+
+db_character_one_user="Bugs"
+db_character_one_password="password123"
+db_character_one_is_admin="FALSE"
+db_character_one_firstname="Bugs"
+db_character_one_lastname="Bunny"
+db_character_one_signature="Eh, What\'s up doc?"
+
+#Character_two#
+
+db_character_two_user="Lola"
+db_character_two_password="password123"
+db_character_two_is_admin="FALSE"
+db_character_two_firstname="Lola"
+db_character_two_lastname="Bunny"
+db_character_two_signature="Don't ever call me \'Doll."
+
+
+#Character_three
+db_character_three_user="Daffy"
+db_character_three_password="password123"
+db_character_three_is_admin="FALSE"
+db_character_three_firstname="Daffy"
+db_character_three_lastname="Duck"
+db_character_three_signature="I think you\'re pretty tough don\'t I?"
+
+#Character_four
+db_character_four_user="Road"
+db_character_four_password="password123"
+db_character_four_is_admin="FALSE"
+db_character_four_firstname="Road"
+db_character_four_lastname="Runner"
+db_character_four_signature="Meep! Meep!"
+
+
+#Character_five
+db_character_five_user="Porky"
+db_character_five_password="password123"
+db_character_five_is_admin="FALSE"
+db_character_five_firstname="Porky"
+db_character_five_lastname="Pig"
+db_character_five_signature="That\'s all folks!"
+
+
+#Character_six
+db_character_six_user="Wile_E"
+db_character_six_password="password123"
+db_character_six_is_admin="TRUE"
+db_character_six_firstname="Wile E"
+db_character_six_lastname="Coyote"
+db_character_six_signature="*HOLDS UP A SIGN WITH THE WORDS \'HELP\'*."
+
+#Character_seven
+db_character_seven_user="Foghorn"
+db_character_seven_password="password123"
+db_character_seven_is_admin="FALSE"
+db_character_seven_firstname="Foghorn"
+db_character_seven_lastname="Leghorn"
+db_character_seven_signature="I looka, I say, looka here."
+
+
+#Webserver variables
+SITE_NAME="ACME Factory"
+
+WEBSITE_CREATED_BY="This website was created by the Tasmanian Devil - 2023"
+
+WELCOME="Welcome to the ACME factories webpage, thank you for visiting our site."
+
+CONTACT_EMAIL_ONE="Bugs@ACME-factory.com"
+CONTACT_EMAIL_TWO="Lola@ACME-factory.com"
+CONTACT_EMAIL_THREE="Daffy@ACME-factory.com"
+CONTACT_EMAIL_FOUR="Road@ACME-factory.com"
+CONTACT_EMAIL_FIVE="Porky@ACME-factory.com"
+CONTACT_EMAIL_SIX="Wile_E@ACME-factory.com"
+CONTACT_EMAIL_SEVEN="Foghorn@ACME-factory.com"
+
+ABOUT_ONE="The ACME factory specialises in the creation a wide range of"
+ABOUT_TWO="products ranging from rockets to rather heavy pianos. "
+ABOUT_THREE="Our key shareholder is Wile.E Coyote."
+
+#Secret keys
+
+#Source code secret key (Zer0sh0ck Binary)
+SECRET_FLAG_ONE="01011010 01100101 01110010 00110000 01110011 01101000 00110000 01100011 01101011"
+
+#Secret directory secret key (L0gicB0mbe Hex)
+SECRET_FLAG_TWO="0x4c 0x30 0x67 0x69 0x63 0x42 0x30 0x6d 0x62 0x65"
+
+#SMB enumeration secret key (3301 base64)
+SECRET_FLAG_THREE="MzMwMQ=="
+
+#Shared drive key secret key (Petabyte octal)
+SECRET_FLAG_FOUR="120 145 164 141 142 171 164 145 012"
+
+#SQL injection secret key (Synergists decimal)
+SECRET_FLAG_FIVE="83 121 110 101 114 103 105 115 116 115 10"
+
+
+root@Hogwarts:/home/admin_account#
+root@Hogwarts:/home/admin_account# cat Chocolate_factory_variables.sh
+#!/usr/bin/env bash
+
 #CTF Machine details
 
 HOSTNAME="Chocolate_Factory"
@@ -15,13 +144,13 @@ character_five="Hellen"
 character_six="Oompa_Lompa"
 character_seven="Squrriel"
 
-password_one="password123"
-password_two="password123"
-password_three="password123"
-password_four="password123"
-password_five="password123"
-password_six="password123"
-password_seven="password123"
+password_one='password123'
+password_two='password123'
+password_three='password123'
+password_four='password123'
+password_five='password123'
+password_six='password123'
+password_seven='password123'
 
 #Here is the Database information variables
 
@@ -122,5 +251,3 @@ SECRET_FLAG_FOUR="120 145 164 141 142 171 164 145 012"
 
 #SQL injection secret key (Synergists decimal)
 SECRET_FLAG_FIVE="83 121 110 101 114 103 105 115 116 115 10"
-
-
